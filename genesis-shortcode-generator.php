b8af865b8ede61b4a2d4937029b98fdd16ee4626
BeardedGinger <josh@limecuda.com>
2016-02-03 03:49:38 -0500

    plugin layout start

---
+++ genesis-shortcode-generator.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name:       Genesis Shortcode Generator
+ * Plugin URI:        http://joshmallard.com
+ * Description:       Supercharge the possibilities for WordPress content without bombarding users with thousands of options they'll never need or use
+ * Version:           2.0.0
+ * Author:            Josh Mallard
+ * Author URI:        http://joshmallard.com
+ * Text Domain:       gingerbeard-shortcodes
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
